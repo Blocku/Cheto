bcd28f1c1b581ba1cbc846c65bbb15ab101bbba8
Blocku <126176292+Blocku@users.noreply.github.com>
2024-03-20 15:21:12 +0000

    1

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<header class="fixed w-full h-auto flex items-center p-20 border border-primary-300 " >
+    <div class="w-[60%] h-auto flex justify-between items-centers border border-black " >
+        <a href="/">
+        </a>
+        <nav>
+            <ul class="flex space-x-20" >
+                <li><a href="/">Наличие</a></li>
+                <li><a href="/">Доставка</a></li>
+                <li><a href="/">Отзывы</a></li>
+                <li><a href="/">Характеристики</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class=" w-[40%] h-auto border border-zinc-300" >
+
+    </div>
+</header>
